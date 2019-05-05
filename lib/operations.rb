17220322bbfac8985b168a56e6e2404337cd3cc8
Haley DeKeyser <hcd09@live.com>
2019-05-05 02:44:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,8 @@
 def unsafe?(speed)
-  if speed > 60
+  if speed > 60 || speed < 40
     return true
-  elsif speed < 40
-    return true
-  else 40 >= speed <= 60
-    return false
+  else (speed >= 40) && (speed <= 60)
+  end
 end
 
 
